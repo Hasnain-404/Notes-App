04c46a7df014e462d13c5bdb623e896b5628bf92
Hasnain <devxpert404@gmail.com>
2025-04-05 22:11:36 +0530

    change oraging in app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5137",
+    origin: "https://notesapp-l2a7.onrender.com",
     credentials: true,
 }))
 app.use(clerkMiddleware())
