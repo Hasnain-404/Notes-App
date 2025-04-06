14ec1accac5aaf880ac6bbf632deff6141e03cb5
Hasnain <devxpert404@gmail.com>
2025-04-06 09:18:09 +0530

    check userId for test

--- backend/middlewares/userMiddleware.js
+++ backend/middlewares/userMiddleware.js
@@ -4,6 +4,9 @@ export const userAuth = (req, res, next) => {
     try {
         const { userId } = getAuth(req);
 
+        console.log("User ID from Clerk:", userId); // Log the user ID for debugging
+
+
         if (!userId) {
             return res.status(401).json({ message: 'Unauthorized access. Please log in.' });
         }
